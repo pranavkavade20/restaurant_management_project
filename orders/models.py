ae20ded7192a2dcd0f0d99319f1d2a81dff3a1a7
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-04 13:34:24 +0530

    Update models.py
    order model is updated. User model is moving to accounts app.

--- orders/models.py
+++ orders/models.py
@@ -1,23 +1,5 @@
 from django.db import models
 from django.core.validatiors import RegexValidator
-from django.contrib.auth.models import User
-
-#User Profile model for saving information of authenticated users.
-class UserProfile(models.Model):
-    # Save the name of user.
-    name = models.CharField(max_length=50)
-    # Email of user for logging.     
-    email = models.EmailField(max_length=25, unique=True)
-    # Phone number of user with validation/
-    phone_number = models.CharField(
-        max_length=20, # Adjust the length of phone number digits.
-        validatiors=[
-            RegexValidator(
-                regex=r'^\+?1?\d{9,15}$', # regex for international phone numbers
-                message = "Phone number must be entered in the format : +9999999999. Up to digits allowed."
-            )
-        ]
-    )
 
 # Menu model create for store menu.
 class Menu(modes.Model):
@@ -74,4 +56,5 @@ class OrderItem(models.Model):
     
     def get_item_total(self):
         # Returns total price of menu_item with help of quantity.
+
         return self.menu_item.price * self.quantity
