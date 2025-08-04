ddaeade75acb20c6d26fe2803ecbd10eb1f62e22
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-04 11:47:06 +0530

    19161

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validatiors import RegexValidator
 
-# Create your models here.
+"""
+User Profile model for saving information of authenticated users.
+"""
+class UserProfile(models.Model):
+    # Save the name of user.
+    name = models.CharField(max_length=50)
+    # Email of user for logging.     
+    email = models.EmailField(max_length=25, unique=True)
+    # Phone number of user with validation/
+    phone_number = models.CharField(
+        max_length=15, # Adjust the length of phone number digits.
+        validatiors=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$', # regex for international phone numbers
+                message = "Phone number must be entered in the format : +9999999999. Up to digits allowed."
+            )
+        ],
+        unique=True
+    )
