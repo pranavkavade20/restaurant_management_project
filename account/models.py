6d27df8b2dc09cc39fb76ea619176689d7c4f245
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-04 13:36:03 +0530

    Update models.py
    Moved UserProfile model is  added in accounts app that handle a users.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#User Profile model for saving information of authenticated users.
+class UserProfile(models.Model):
+    # Save the name of user.
+    name = models.CharField(max_length=50)
+    # Email of user for logging.     
+    email = models.EmailField(max_length=25, unique=True)
+    # Phone number of user with validation/
+    phone_number = models.CharField(
+        max_length=20, # Adjust the length of phone number digits.
+        validatiors=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$', # regex for international phone numbers
+                message = "Phone number must be entered in the format : +9999999999. Up to digits allowed."
+            )
+        ]
+    )
+
