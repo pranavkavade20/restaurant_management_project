0a87c30747eabd7b88b986c25d8a2846c6e069e3
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-05 10:59:55 +0530

    19164

--- products/models.py
+++ products/models.py
@@ -9,3 +9,10 @@ class Item(models.Model):
     def __str__(self):
         return str(self.item_name)
 
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
