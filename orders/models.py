f6d655f3b392b377dcc8ca58b36fb127a1531a25
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-05 10:40:41 +0530

    19164

--- orders/models.py
+++ orders/models.py
@@ -2,33 +2,6 @@ from django.db import models
 from django.core.validatiors import RegexValidator
 from django.contrib.auth.models import User
 
-#User Profile model for saving information of authenticated users.
-class UserProfile(models.Model):
-    # Save the name of user.
-    name = models.CharField(max_length=50)
-    # Email of user for logging.     
-    email = models.EmailField(max_length=25, unique=True)
-    # Phone number of user with validation/
-    phone_number = models.CharField(
-        max_length=20, # Adjust the length of phone number digits.
-        validatiors=[
-            RegexValidator(
-                regex=r'^\+?1?\d{9,15}$', # regex for international phone numbers
-                message = "Phone number must be entered in the format : +9999999999. Up to digits allowed."
-            )
-        ]
-    )
-
-# Menu model create for store menu.
-class Menu(modes.Model):
-    # Store Menu Item.
-    name = models.CharField(max_length=150)
-    # Store Price.
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-
-    def __str_(self):
-        return self.name
-
 # Order model that store the orders.
 class Order(models.Model):
     # Choices that show status realted to order_status.
