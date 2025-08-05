cfbfa569057516b4aeab3a62e8c80f4d6d70c529
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-05 18:26:14 +0530

    19162

--- home/views.py
+++ home/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 import requests
 
+# View for fetching API Response.
 def menu_view(request):
     try:
+        # Getting Response throught a URL.
         response = requests.get('http://localhost:8000/api/products/menu/')
+        # Saving response in JSON format.
         menu_data = response.json()
     except Exception as e:
+        # If exception occur then it save empty response.
         menu_data=[]
-    
+    # Render data to frontend.
     return render(request, 'home/menu.html',{'menu':menu_data})
 
