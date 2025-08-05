551d482f397434fcbb3fd27cfdea264786b8cd3c
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-05 18:24:14 +0530

    19162

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def menu_view(request):
+    try:
+        response = requests.get('http://localhost:8000/api/products/menu/')
+        menu_data = response.json()
+    except Exception as e:
+        menu_data=[]
+    
+    return render(request, 'home/menu.html',{'menu':menu_data})
+
