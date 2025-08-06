8acdb9125091e5e8ba6dacb61d92f7d80090c7f7
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-06 11:01:35 +0530

    22531

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', menu_view, name='home')
+    path('', menu_view, name='home'),
+    path('menu/' menu_view,name='menu')
 ]
