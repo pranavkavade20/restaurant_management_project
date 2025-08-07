9704b333052d014a4b2ead0c5e0b8c3c28299e5c
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-07 13:45:40 +0530

    22533

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -131,5 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Restaurant Name
 RESTAURANT_NAME="Ganesh Restaurant"
 
-DEBUG = False
 ALLOWED_HOSTS =['*']
