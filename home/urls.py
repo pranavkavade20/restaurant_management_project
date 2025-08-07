61d91dd1cd9cf4ee29496fe5bd8155cd70408fcf
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-07 13:41:47 +0530

    22533

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', menu_view, name='home'),
-    path('menu/' menu_view,name='menu'),
-    path('404-test/',views.trigger_404)
+    path('menu/' menu_view, name='menu'),
+    path('404-test/', trigger_404, name="trigger_404"),
+    path('about/', about_view, name="about"),
 ]
