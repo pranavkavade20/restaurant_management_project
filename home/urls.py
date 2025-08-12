5ce2bbf249ca2f13f9ecf9fff304e512fa058e68
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-12 13:04:41 +0530

    27905

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('menu/' menu_view, name='menu'),
     path('404-test/', trigger_404, name="trigger_404"),
     path('about/', about_view, name="about"),
+    path('contact/', contact_us, name="contact"),
 ]
