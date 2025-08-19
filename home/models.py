8dc15455c831a331e29279c6a505e6b94a451656
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-19 11:55:18 +0530

    34936

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Feedback model
+class Feedback(models.Model):
+    # Save comment
+    comment = models.TextField()
+    # Save current time of comment.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # return first 40 characters on admin dashboard.
+        return self.comment[:40] or "Feedback"
