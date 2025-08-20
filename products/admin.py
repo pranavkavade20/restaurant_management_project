c00ebf34649e141eb87196104f7b6de007cbfafd
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-20 11:31:14 +0530

    35741

--- products/admin.py
+++ products/admin.py
@@ -6,6 +6,9 @@ from .models import *
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
 
-
-# Register your models here.
 admin.site.register(Item,ItemAdmin)
+
+# Menu model registeration.
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("name","price")
