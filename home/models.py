23126bff22c7e8ae2e867f5138d0a8b4e7d6658b
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-20 12:35:43 +0530

    35738

--- home/models.py
+++ home/models.py
@@ -10,3 +10,12 @@ class Feedback(models.Model):
     def __str__(self):
         # return first 40 characters on admin dashboard.
         return self.comment[:40] or "Feedback"
+
+# Contact model for Home page.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
