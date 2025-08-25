9af295c6793637e4bbd78a25c6ec8245d4b5516e
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-25 09:47:27 +0530

    38809

--- home/models.py
+++ home/models.py
@@ -19,3 +19,15 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+# Restaurant Address model.
+class Address(models.Model):
+    address = models.CharField(max_length=255) # Street Address
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    # Kept as CharField to allow codes like "02115" or "12345-67890"
+    zip_code = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now=True) # It save the time when last updated address.
+
+    def __str__(self):
+        return f"{self.address},{self.city}, {self.state} {self.zip_code}"
