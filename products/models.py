b1f2786f4fe7c85340428d9d9b318cb8088f6acc
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-27 19:45:48 +0530

    38806

--- products/models.py
+++ products/models.py
@@ -14,7 +14,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8,decimal_places=2) # Save Price in decimal places 2.
-    image_url = models.URLField(blank=True,null=True) # Optional field
-    
+    image = models.ImageField(upload_to="menu_images/",blank=True,null=True) # Image upload on local storage.
+
     def __str__(self):
         return f"{self.name} --> {self.price}"
