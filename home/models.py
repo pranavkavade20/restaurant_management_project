6f56478f6b883c16050a1833249ee89f5607bdc9
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-08-28 11:13:14 +0530

    38808

--- home/models.py
+++ home/models.py
@@ -16,7 +16,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField(blank=True)
-    submitted_at = models.DateTimeField(auto_now_add=True)
+    submitted_at = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return f"{self.name} ({self.email})"
