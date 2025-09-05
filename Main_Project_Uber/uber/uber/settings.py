264ff06d844cf8b64f0792ddb69d069e81c3cef4
PranavGithubUser <pranavkavade9@gmail.com>
2025-09-05 10:44:39 +0530

    static and media folder updated

--- Main_Project_Uber/uber/uber/settings.py
+++ Main_Project_Uber/uber/uber/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 STATIC_URL = '/static/' # below this add the following line
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
