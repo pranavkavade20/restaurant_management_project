f337976322809ed38199b495044eedfc520e3f70
Pranav <123260105+pranavkavade20@users.noreply.github.com>
2025-10-16 11:35:17 +0530

    55421

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
+    
     path('MenuItems/', MenuItemView.as_view(), name='MenuItem-list'),
     path("menu-items/by-category/", MenuItemsByCategoryAPIView.as_view(), name="menu-items-by-category"),
     path("menu-items/<int:pk>/update/", UpdateMenuItemAPIView.as_view(), name="update-menu-item"),
